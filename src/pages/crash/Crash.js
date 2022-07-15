a2843644a4d42c2eca588c34293c228f5a7102cd
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2022-07-15 01:20:24 -0300

    rollback: adjustments rollback after found vercel app problem was env variables

--- src/pages/crash/Crash.js
+++ src/pages/crash/Crash.js
@@ -1,4 +1,4 @@
-import React, { useMemo }  from "react";
+import React  from "react";
 import Loader from "../../components/Loader/Loader";
 import useCrashes from "../../hooks/useCrashes";
 import Card from "./components/Card/Card";
@@ -7,19 +7,14 @@ import Page from "./Crash.styles";
 export default function CrashPage() {
   const { crashes } = useCrashes();
 
-  const memoizedCrashes = useMemo(() => {
-    if (!crashes) return [];
-    return crashes?.slice(0, 1000);
-  }, [crashes]);
-
   return (
     <Page.Container>
       <h1>Blaze Crash</h1>
       <Page.Flex>
-        {!memoizedCrashes?.length && (<Loader />)}
-        {memoizedCrashes?.map((el, index) => {
-          return <Card key={`${index}_Card`} data={el} id={crashes?.length - index} />
-        })}
+        {!crashes?.length && (<Loader />)}
+        {crashes.map((el, index) => (
+          <Card key={`${index}_Card`} data={el} id={crashes?.length - index} />
+        ))}
       </Page.Flex>
     </Page.Container>
   );
