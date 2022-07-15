6e261e5a2b569e83be36a25fc0ceffa0005621c1
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2022-07-15 00:54:22 -0300

    debug: added console.log

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,7 @@
 import socketIo from "socket.io-client";
 
+console.log({socket: process.env.REACT_APP_SOCKET_URL});
+
 const socket = socketIo(process.env.REACT_APP_SOCKET_URL, {transports: ['websocket']});
 
 export default socket;
